f6466a90dbf34172ba78dfbe7e65edb9bbe97eb2
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2005-07-29 12:46:45 +0000

    *** empty log message ***
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@675 516812f1-d3fd-0310-9ea4-9aededebcb87

--- rsplib/fractalexample/fractalpooluser.cc
+++ rsplib/fractalexample/fractalpooluser.cc
@@ -348,6 +348,7 @@ void FractalPU::run()
    for(;;) {
       LastPoolElementID = 0;
 
+      /*
       tags[0].Tag  = TAG_TuneSCTP_MinRTO;
       tags[0].Data = 200;
       tags[1].Tag  = TAG_TuneSCTP_MaxRTO;
@@ -361,6 +362,8 @@ void FractalPU::run()
       tags[5].Tag  = TAG_TuneSCTP_AssocMaxRXT;
       tags[5].Data = 12;
       tags[6].Tag  = TAG_DONE;
+      */
+      tags[0].Tag  = TAG_DONE;
 
       Session = rspCreateSession(PoolHandle, PoolHandleSize, NULL, (TagItem*)&tags);
       if(Session) {
