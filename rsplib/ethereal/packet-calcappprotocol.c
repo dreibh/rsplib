1e899f784b6c92b5072eafc5bd8acd169629140b
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2005-10-26 15:36:20 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@874 516812f1-d3fd-0310-9ea4-9aededebcb87

--- rsplib/ethereal/packet-calcappprotocol.c
+++ rsplib/ethereal/packet-calcappprotocol.c
@@ -5,12 +5,14 @@
 #include <epan/packet.h>
 
 
-#define CALCAPPPROTOCOL_PAYLOAD_PROTOCOL_ID 0x55555555
+#define CALCAPPPROTOCOL_PAYLOAD_PROTOCOL_ID 0x29097603
 
 
 /* Initialize the protocol and registered fields */
 static int proto_calcappprotocol = -1;
 static int hf_message_type       = -1;
+static int hf_message_flags      = -1;
+static int hf_message_length     = -1;
 static int hf_message_jobid      = -1;
 static int hf_message_jobsize    = -1;
 static int hf_message_completed  = -1;
@@ -25,13 +27,17 @@ dissect_calcappprotocol_message(tvbuff_t *, packet_info *, proto_tree *);
 
 
 /* Dissectors for messages. This is specific to CalcAppProtocol */
-#define MESSAGE_TYPE_LENGTH      4
+#define MESSAGE_TYPE_LENGTH    1
+#define MESSAGE_FLAGS_LENGTH   1
+#define MESSAGE_LENGTH_LENGTH  2
 #define MESSAGE_JOBID_LENGTH     4
 #define MESSAGE_JOBSIZE_LENGTH   8
 #define MESSAGE_COMPLETED_LENGTH 8
 
 #define MESSAGE_TYPE_OFFSET      0
-#define MESSAGE_JOBID_OFFSET     (MESSAGE_TYPE_OFFSET    + MESSAGE_TYPE_LENGTH)
+#define MESSAGE_FLAGS_OFFSET     (MESSAGE_TYPE_OFFSET    + MESSAGE_TYPE_LENGTH)
+#define MESSAGE_LENGTH_OFFSET    (MESSAGE_FLAGS_OFFSET   + MESSAGE_FLAGS_LENGTH)
+#define MESSAGE_JOBID_OFFSET     (MESSAGE_LENGTH_OFFSET    + MESSAGE_LENGTH_LENGTH)
 #define MESSAGE_JOBSIZE_OFFSET   (MESSAGE_JOBID_OFFSET   + MESSAGE_JOBID_OFFSET)
 #define MESSAGE_COMPLETED_OFFSET (MESSAGE_JOBSIZE_OFFSET + MESSAGE_JOBSIZE_OFFSET)
 
@@ -60,14 +66,16 @@ static const value_string message_type_values[] = {
 static void
 dissect_calcappprotocol_message(tvbuff_t *message_tvb, packet_info *pinfo, proto_tree *calcappprotocol_tree)
 {
-  guint32 type;
+  guint8 type;
 
-  type = tvb_get_ntohl(message_tvb, MESSAGE_TYPE_OFFSET);
+  type = tvb_get_guint8(message_tvb, MESSAGE_TYPE_OFFSET);
   if (pinfo && (check_col(pinfo->cinfo, COL_INFO))) {
     col_add_fstr(pinfo->cinfo, COL_INFO, "%s ", val_to_str(type, message_type_values, "Unknown CalcAppProtocol type"));
   }
   if (calcappprotocol_tree) {
     proto_tree_add_item(calcappprotocol_tree, hf_message_type,      message_tvb, MESSAGE_TYPE_OFFSET,      MESSAGE_TYPE_LENGTH,      NETWORK_BYTE_ORDER);
+    proto_tree_add_item(calcappprotocol_tree, hf_message_flags,  message_tvb, MESSAGE_FLAGS_OFFSET,  MESSAGE_FLAGS_LENGTH,  NETWORK_BYTE_ORDER);
+    proto_tree_add_item(calcappprotocol_tree, hf_message_length, message_tvb, MESSAGE_LENGTH_OFFSET, MESSAGE_LENGTH_LENGTH, NETWORK_BYTE_ORDER);
     proto_tree_add_item(calcappprotocol_tree, hf_message_jobid,     message_tvb, MESSAGE_JOBID_OFFSET,     MESSAGE_JOBID_LENGTH,     NETWORK_BYTE_ORDER);
     proto_tree_add_item(calcappprotocol_tree, hf_message_jobsize,   message_tvb, MESSAGE_JOBSIZE_OFFSET,   MESSAGE_JOBSIZE_LENGTH,   NETWORK_BYTE_ORDER);
     proto_tree_add_item(calcappprotocol_tree, hf_message_completed, message_tvb, MESSAGE_COMPLETED_OFFSET, MESSAGE_COMPLETED_LENGTH, NETWORK_BYTE_ORDER);
@@ -104,7 +112,9 @@ proto_register_calcappprotocol(void)
 
   /* Setup list of header fields */
   static hf_register_info hf[] = {
-    { &hf_message_type,      { "Type",      "calcappprotocol.message_type",      FT_UINT32, BASE_DEC, VALS(message_type_values), 0x0, "", HFILL } },
+    { &hf_message_type,      { "Type",      "calcappprotocol.message_type",      FT_UINT8, BASE_DEC, VALS(message_type_values), 0x0, "", HFILL } },
+    { &hf_message_flags,    { "Flags",  "calcppprotocol.message_flags",    FT_UINT8,  BASE_DEC, NULL,                      0x0, "", HFILL } },
+    { &hf_message_length,   { "Length", "calcappprotocol.message_length",   FT_UINT16, BASE_DEC, NULL,                      0x0, "", HFILL } },
     { &hf_message_jobid,     { "JobID",     "calcappprotocol.message_jobid",     FT_UINT32, BASE_DEC, NULL,                      0x0, "", HFILL } },
     { &hf_message_jobsize,   { "JobSize",   "calcappprotocol.message_jobsize",   FT_UINT64, BASE_DEC, NULL,                      0x0, "", HFILL } },
     { &hf_message_completed, { "Completed", "calcappprotocol.message_completed", FT_UINT64, BASE_DEC, NULL,                      0x0, "", HFILL } },
