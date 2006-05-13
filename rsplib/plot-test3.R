7d1a9d7bd30545d30967fc65b42300535543a245
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2006-05-13 21:08:26 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@1112 516812f1-d3fd-0310-9ea4-9aededebcb87

---
+++ rsplib/plot-test3.R
@@ -0,0 +1,98 @@
+source("plotter.R")
+
+hideLegend          <- TRUE
+colorMode           <- cmColor
+legendPos           <- c(1,1)
+
+generateOutput <- function(inFile, resultType, mainTitle="", summary=TRUE, yAxisTicks=c())
+{
+   xSet <- (data$CompleteTimeStamp / 60)
+   xOffset <- -min(xSet)
+   xSet <- xSet + xOffset
+   xTitle <- "Time [Minutes]"
+
+   hbarSet <- (data$QueuingTimeStamp / 60) + xOffset
+   hbarMeanSteps <- 26
+
+   if(resultType =="HandlingTime") {
+      ySet <- data$HandlingTime
+      yTitle <- "Average Request Handling Time [s]"
+   }
+   else if(resultType =="HandlingSpeed") {
+      ySet <- data$HandlingSpeed
+      yTitle <- "Average Request Handling Speed [Calculations/s]"
+   }
+   else {
+      stop("Bad result type!")
+   }
+
+   zSet <- data$ObjectName
+   zTitle <- ""
+   vSet <- c()
+   vTitle <- ""
+   wSet <- c()
+   wTitle <- ""
+
+   xAxisTicks <- getUsefulTicks(xSet)   # getIntegerTicks(seq(0, max(xSet) - 60))   # Set to c() for automatic setting
+   if(length(yAxisTicks) < 2) {
+      yAxisTicks <- getIntegerTicks(ySet, count=10)   # Set to c() for automatic setting
+   }
+
+   if(summary) {
+      plotstd3(mainTitle, xTitle, yTitle, zTitle, xSet, ySet,
+               zSet, vSet, wSet, vTitle, wTitle,
+               hbarSet = hbarSet, hbarMeanSteps = hbarMeanSteps,
+               xSeparatorsSet = xSeparatorsSet, xSeparatorsTitles = xSeparatorsTitles,
+               xAxisTicks = xAxisTicks, yAxisTicks = yAxisTicks,
+               type="h",
+               colorMode = colorMode, hideLegend = hideLegend,
+               legendPos = legendPos)
+   }
+   else {
+      # levels(factor(zSet))
+      for(z in c("PU-11-planetlab1.iii.u-tokyo.ac.jp")) {
+         cat(sep="", "z=", z, "\n")
+         filter <- (zSet == z)
+         xSubset <- subset(xSet, filter)
+         ySubset <- subset(ySet, filter)
+         zSubset <- subset(zSet, filter)
+         vSubset <- subset(vSet, filter)
+         wSubset <- subset(wSet, filter)
+         hbarSubset <- subset(hbarSet, filter)
+         plotstd3(paste(sep="", mainTitle, ", PU=", z),
+                  xTitle, yTitle, zTitle,
+                  xSubset, ySubset,
+                  zSubset, vSubset, wSubset, vTitle, wTitle,
+                  hbarSet = hbarSubset, hbarMeanSteps = hbarMeanSteps,
+                  xSeparatorsSet = xSeparatorsSet, xSeparatorsTitles = xSeparatorsTitles,
+                  xAxisTicks = xAxisTicks, yAxisTicks = yAxisTicks,
+                  type="h",
+                  colorMode = colorMode, hideLegend = hideLegend,
+                  legendPos = legendPos)
+      }
+   }
+}
+
+
+
+pdf("x.pdf", width=11.69, height=8.26, onefile=TRUE, family="Helvetica", pointsize=14)
+
+
+data <- loadResults("messung3/pu-vectors.vec.bz2")
+#data <- loadResults("x.vec.bz2")
+
+f <- (data$HandlingTime > 80)
+c <- subset(data$ObjectName, f)
+cat("LV:",levels(factor(data$ObjectName)),"\n")
+cat("bad:",levels(factor(c)),"\n")
+
+xSeparatorsSet <- c(15, 30, 45, 50)
+xSeparatorsTitles <- c("Failures\nin Asia",
+                        "Backup\nCapacity",
+                        "Reco-\nvery\nComp-\nleted",
+                        "Normal\nOperation")
+generateOutput("messung3/pu-vectors.vec.bz2", "HandlingTime", "Least Used Policy with Delay Penalty Factor",
+               FALSE,
+               seq(0,90,10))
+
+dev.off()
