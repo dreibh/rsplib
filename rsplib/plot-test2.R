324e9d59ff4dae9b0fbfdd8706133bff785c5771
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2006-05-13 09:33:29 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@1106 516812f1-d3fd-0310-9ea4-9aededebcb87

--- rsplib/plot-test2.R
+++ rsplib/plot-test2.R
@@ -4,14 +4,7 @@ hideLegend          <- TRUE
 colorMode           <- cmColor
 legendPos           <- c(1,1)
 
-xSeparatorsSet <- c(15, 30, 45, 50)
-xSeparatorsTitles <- c("Failures\nin Asia",
-                        "Backup\nCapacity",
-                        "Reco-\nvery\nComp-\nleted",
-                        "Normal\nOperation")
-
-
-generateOutput <- function(inFile, resultType, mainTitle="")
+generateOutput <- function(inFile, resultType, mainTitle="", summary=TRUE)
 {
 
    data <- loadResults(inFile)
@@ -46,26 +39,62 @@ generateOutput <- function(inFile, resultType, mainTitle="")
    xAxisTicks <- getUsefulTicks(xSet)   # getIntegerTicks(seq(0, max(xSet) - 60))   # Set to c() for automatic setting
    yAxisTicks <- getIntegerTicks(ySet, count=10)   # Set to c() for automatic setting
 
-   plotstd3(mainTitle, xTitle, yTitle, zTitle, xSet, ySet,
-            zSet, vSet, wSet, vTitle, wTitle,
-            hbarSet = hbarSet, hbarMeanSteps = hbarMeanSteps,
-            xSeparatorsSet = xSeparatorsSet, xSeparatorsTitles = xSeparatorsTitles,
-            xAxisTicks = xAxisTicks, yAxisTicks = yAxisTicks,
-            type="h",
-            colorMode = colorMode, hideLegend = hideLegend,
-            legendPos = legendPos)
+   if(summary) {
+      plotstd3(mainTitle, xTitle, yTitle, zTitle, xSet, ySet,
+               zSet, vSet, wSet, vTitle, wTitle,
+               hbarSet = hbarSet, hbarMeanSteps = hbarMeanSteps,
+               xSeparatorsSet = xSeparatorsSet, xSeparatorsTitles = xSeparatorsTitles,
+               xAxisTicks = xAxisTicks, yAxisTicks = yAxisTicks,
+               type="h",
+               colorMode = colorMode, hideLegend = hideLegend,
+               legendPos = legendPos)
+   }
+   else {
+      for(z in levels(factor(zSet))) {
+         cat(sep="", "z=", z, "\n")
+         filter <- (zSet == z)
+         xSubset <- subset(xSet, filter)
+         ySubset <- subset(ySet, filter)
+         zSubset <- subset(zSet, filter)
+         vSubset <- subset(vSet, filter)
+         wSubset <- subset(wSet, filter)
+         hbarSubset <- subset(hbarSet, filter)
+         plotstd3(paste(sep="", mainTitle, ", PU=", z),
+                  xTitle, yTitle, zTitle, xSet, ySet,
+                  zSubset, vSubset, wSubset, vTitle, wTitle,
+                  hbarSet = hbarSubset, hbarMeanSteps = hbarMeanSteps,
+                  xSeparatorsSet = xSeparatorsSet, xSeparatorsTitles = xSeparatorsTitles,
+                  xAxisTicks = xAxisTicks, yAxisTicks = yAxisTicks,
+                  type="h",
+                  colorMode = colorMode, hideLegend = hideLegend,
+                  legendPos = legendPos)
+      }
+   }
 }
 
 
 
 pdf("test2.pdf", width=11.69, height=8.26, onefile=TRUE, family="Helvetica", pointsize=14)
 
+xSeparatorsSet <- c()
+xSeparatorsTitles <- c()
 # generateOutput("x.vec.bz2", "HandlingTime")
 # generateOutput("x.vec.bz2", "HandlingSpeed")
 
+
+xSeparatorsSet <- c(15, 30, 45, 50)
+xSeparatorsTitles <- c("Failures\nin Asia",
+                        "Backup\nCapacity",
+                        "Reco-\nvery\nComp-\nleted",
+                        "Normal\nOperation")
 generateOutput("messung3/pu-vectors.vec.bz2", "HandlingTime", "Least Used Policy with Delay Penalty Factor")
-generateOutput("messung3/pu-vectors.vec.bz2", "HandlingSpeed", "Least Used Policy with Delay Penalty Factor")
 generateOutput("messung4/pu-vectors.vec.bz2", "HandlingTime", "Least Used Policy")
+generateOutput("messung3/pu-vectors.vec.bz2", "HandlingSpeed", "Least Used Policy with Delay Penalty Factor")
 generateOutput("messung4/pu-vectors.vec.bz2", "HandlingSpeed", "Least Used Policy")
+
+generateOutput("messung3/pu-vectors.vec.bz2", "HandlingTime", "Least Used Policy with Delay Penalty Factor", FALSE)
+generateOutput("messung4/pu-vectors.vec.bz2", "HandlingTime", "Least Used Policy", FALSE)
+generateOutput("messung3/pu-vectors.vec.bz2", "HandlingSpeed", "Least Used Policy with Delay Penalty Factor", FALSE)
+generateOutput("messung4/pu-vectors.vec.bz2", "HandlingSpeed", "Least Used Policy", FALSE)
 
 dev.off()
