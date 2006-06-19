155ad1701530ac338afbdc87248ae5cea4410ecd
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2006-06-19 15:41:26 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@1159 516812f1-d3fd-0310-9ea4-9aededebcb87

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./bootstrap && ./configure --enable-static --disable-shared --enable-qt --enable-csp $@ && gmake
+./bootstrap && ./configure --enable-static --disable-shared --enable-qt --enable-csp $@ && make
