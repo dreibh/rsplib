998b76c3c6c38baeca077d1fe3bc8db735ee0907
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2007-06-16 21:57:01 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@1466 516812f1-d3fd-0310-9ea4-9aededebcb87

--- rsplib/run-perftest1.R
+++ rsplib/run-perftest1.R
@@ -11,7 +11,7 @@ testName              <- "P01"
 minPreSkip            <- 30
 minPostSkip           <- 30
 segmentLength         <- 30
-segments              <- 2
+segments              <- 12
 
 prHosts               <- c("132.252.151.178", "132.252.151.157")
 peHosts               <- c("132.252.152.70", "132.252.152.71", "132.252.152.72",
@@ -21,7 +21,7 @@ peHosts               <- c("132.252.152.70", "132.252.152.71", "132.252.152.72",
 puHosts               <- peHosts
 
 PRsSet                <- c(2)
-PEsSet                <- c(10, 1000, 3000) # 100, 500, 1000, 1500, 2000, 2500, 2750, 3000, 3250, 3500)
+PEsSet                <- c(10, 100, 500, 1000, 1500, 2000, 2500, 2750, 3000)
 PUsSet                <- c(-1)   # Calculated from puToPERatio
 primaryPERegistrarSet <- c(1)
 primaryPURegistrarSet <- c(1)
--- rsplib/run-perftest2.R
+++ rsplib/run-perftest2.R
@@ -11,7 +11,7 @@ testName              <- "P02"
 minPreSkip            <- 30
 minPostSkip           <- 30
 segmentLength         <- 30
-segments              <- 2
+segments              <- 12
 
 prHosts               <- c("132.252.151.178", "132.252.151.157")
 peHosts               <- c("132.252.152.70", "132.252.152.71", "132.252.152.72",
@@ -21,13 +21,13 @@ peHosts               <- c("132.252.152.70", "132.252.152.71", "132.252.152.72",
 puHosts               <- peHosts
 
 PRsSet                <- c(2)
-PEsSet                <- c(1000)   # 10, 100, 1000)
-PUsSet                <- c(100, 1000, 2000) # 250, 500, 750, 1000, 1250, 1500, 1750, 2000, xxx2250xxx)
+PEsSet                <- c(1000)
+PUsSet                <- c(100, 250, 500, 750, 1000, 1250, 1500, 1750, 2000)
 primaryPERegistrarSet <- c(1)
 primaryPURegistrarSet <- c(1, 2)
 puToPERatioSet        <- c(-1)   # Set PUs directly
 reregIntervalSet      <- c(1000)
-interHResTimeSet      <- c(100, 250) # 100, 250, 1000)
+interHResTimeSet      <- c(100, 250)  # , 1000)
 maxHResItemsSet       <- c(1)
 
 
