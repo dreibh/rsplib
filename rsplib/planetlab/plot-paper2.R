2c9ef74010a95b94066e029c66bec6ce90c58295
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2008-11-04 09:30:42 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@1990 516812f1-d3fd-0310-9ea4-9aededebcb87

---
+++ rsplib/planetlab/plot-paper2.R
@@ -0,0 +1,50 @@
+# ###########################################################################
+# Name:        plot-paper1
+# Description: Paper Plots
+# Revision:    $Id$
+# ###########################################################################
+
+source("attack-plottertemplates.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+plotColorMode         <- cmColor
+plotHideLegend        <- FALSE
+plotLegendSizeFactor  <- 0.8
+plotOwnOutput         <- TRUE
+plotFontFamily        <- "Helvetica"
+plotFontPointsize     <- 22
+plotWidth             <- 10
+plotHeight            <- 10
+plotConfidence        <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   list("attack-registration-planetlabI", "attack-registration-planetlabI-HandlingSpeed.pdf",
+        "Pool Element Attack (Lab Measurement)", NA, list(seq(0,85,10)), list(1,0),
+        "AttackInterval", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", ""),
+   list("attack-registration-countermeasure-planetlabII", "attack-registration-countermeasure-planetlabII-HandlingSpeed.pdf",
+        "Lab Measurement", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
+        "Attackers", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", "AttackInterval"),
+
+   list("attack-handleResolution-planetlabI", "attack-handleResolution-planetlabI-HandlingSpeed.pdf",
+        "Pool User Attack (Lab Measurement)", NA,  list(seq(0,85,10)), list(1,0),
+        "AttackInterval", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", "AttackReportUnreachableProbability"),
+   list("attack-handleResolution-countermeasure-planetlabI", "attack-handleResolution-countermeasure-planetlabI-HandlingSpeed.pdf",
+        "Lab Measurement", NA,  list(seq(0,85,10)), list(1,0),
+        "AttackInterval", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", "AttackReportUnreachableProbability"),
+   list("attack-handleResolution-countermeasure-planetlabII", "attack-handleResolution-countermeasure-planetlabII-HandlingSpeed.pdf",
+        "Lab Measurement", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
+        "Attackers", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", "AttackReportUnreachableProbability", "AttackInterval")
+)
+
+# ###########################################################################
+
+createPlots(measurementDirectory, plotConfigurations)
