6c8dc5b10546bfd943e17a0f4fdd01ebbeee1f17
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2008-11-07 21:21:08 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@2003 516812f1-d3fd-0310-9ea4-9aededebcb87

--- rsplib/planetlab/plot-paper2.R
+++ rsplib/planetlab/plot-paper2.R
@@ -23,25 +23,25 @@ plotConfidence        <- 0.95
 
 # ------ Plots --------------------------------------------------------------
 plotConfigurations <- list(
-   list("attack-registration-planetlabII", "attack-registration-planetlabII-HandlingSpeed.pdf",
-        "Pool Element Attack (PlanetLab)", list(seq(0,1,0.1)), list(seq(0,85,10)), list(1,0),
+   list("attack-registration-planetlabI", "attack-registration-planetlabI-HandlingSpeed.pdf",
+        "Pool Element Attack (PlanetLab)", list(seq(0,1,0.1)), list(seq(0,85,10)), list(1,1),
         "AttackFrequency", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
         "Policy", ""),
-   list("attack-registration-countermeasure-planetlabIII", "attack-registration-countermeasure-planetlabIII-HandlingSpeed.pdf",
-        "PlanetLab", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
+   list("attack-registration-countermeasure-planetlabII", "attack-registration-countermeasure-planetlabII-HandlingSpeed.pdf",
+        "Pool Element Attack (PlanetLab)", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
         "Attackers", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
         "Policy", "AttackFrequency"),
 
-   list("attack-handleResolution-planetlabII", "attack-handleResolution-planetlabII-HandlingSpeed.pdf",
-        "Pool User Attack (PlanetLab)", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
+   list("attack-handleResolution-planetlabI", "attack-handleResolution-planetlabI-HandlingSpeed.pdf",
+        "Pool User Attack (PlanetLab)", list(seq(0,2,0.2)),  list(seq(0,85,10)), list(1,0.9),
+        "AttackFrequency", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
+        "Policy", "AttackReportUnreachableProbability"),
+   list("attack-handleResolution-countermeasure-planetlabI", "attack-handleResolution-countermeasure-planetlabI-HandlingSpeed.pdf",
+        "Pool User Attack (PlanetLab)", list(seq(0,2,0.2)),  list(seq(0,85,10)), list(1,0),
         "AttackFrequency", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
         "Policy", "AttackReportUnreachableProbability"),
    list("attack-handleResolution-countermeasure-planetlabII", "attack-handleResolution-countermeasure-planetlabII-HandlingSpeed.pdf",
-        "PlanetLab", list(seq(0,2,0.1)),  list(seq(0,85,10)), list(1,0),
-        "AttackFrequency", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
-        "Policy", "AttackReportUnreachableProbability"),
-   list("attack-handleResolution-countermeasure-planetlabIII", "attack-handleResolution-countermeasure-planetlabIII-HandlingSpeed.pdf",
-        "PlanetLab", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
+        "Pool User Attack (PlanetLab)", list(seq(0,10,1)),  list(seq(0,85,10)), list(1,0),
         "Attackers", "calcAppPoolUser-CalcAppPUAverageHandlingSpeedPercent",
         "Policy", "AttackReportUnreachableProbability", "AttackFrequency")
 )
