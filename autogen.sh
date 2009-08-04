1f2168856e8e583d9b8a2d8caf77ad740620e5ee
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2009-08-04 07:30:05 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@2179 516812f1-d3fd-0310-9ea4-9aededebcb87

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./bootstrap && \
-./configure --disable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --enable-hsmgtverify $@ && \
+./configure --disable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --disable-hsmgtverify $@ && \
 ( gmake || make )
