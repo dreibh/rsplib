bc3254189fcf490a483701003883fb6591854746
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2012-07-03 21:18:38 +0000

    Oversatt FractalPoolUser strenger på norsk!
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@2697 516812f1-d3fd-0310-9ea4-9aededebcb87

---
+++ rsplib/fractalpooluser_no_NO.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nb_NO">
+<context>
+    <name>FractalCalculationThread</name>
+    <message>
+        <location filename="fractalpooluser.cc" line="649"/>
+        <source>Requesting calculation ...</source>
+        <translation type="unfinished">Ber beregning ...</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="707"/>
+        <source>Processed packet #</source>
+        <translation type="unfinished">Behandlet pakke#</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="709"/>
+        <source>of PE</source>
+        <translation type="unfinished">av PE</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="746"/>
+        <source>Image completed!</source>
+        <translation type="unfinished">Bilden ferdigstilt!</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="747"/>
+        <source>Image calculation failed!</source>
+        <translation type="unfinished">Beregning av bildet er mislykket! </translation>
+    </message>
+</context>
+<context>
+    <name>FractalPU</name>
+    <message>
+        <location filename="fractalpooluser.cc" line="141"/>
+        <source>Welcome to FractalPoolUser!</source>
+        <translation type="unfinished">Velkommen til FractalPoolUser!</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="273"/>
+        <source>Starting job distribution ...</source>
+        <translation type="unfinished">Starter beregning jobb distribusjon ... </translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="345"/>
+        <source>Waiting for job completion ...</source>
+        <translation type="unfinished">Venter på beregning jobb ferdigstillelse ...</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="392"/>
+        <source>Image completed</source>
+        <translation type="unfinished">Bilden ferdigstilt</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="392"/>
+        <source>Image calculation failed</source>
+        <translation type="unfinished">Beregning av bildet er mislykket</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="393"/>
+        <source>Waiting for </source>
+        <translation type="unfinished">Vente i </translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="393"/>
+        <source> seconds ...</source>
+        <translation type="unfinished">sekunder ...</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="433"/>
+        <source>Show &amp;Failovers</source>
+        <translation type="unfinished">Viser &amp;failoverer </translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="439"/>
+        <source>Show &amp;Sessions</source>
+        <translation type="unfinished">Viser &amp;sesjoner</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="445"/>
+        <source>Sessions</source>
+        <translation type="unfinished">Sesjoner</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="464"/>
+        <source>&amp;Next Image</source>
+        <translation type="unfinished">&amp;Neste bilde</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="465"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">&amp;Om</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="467"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">&amp;Avslutt</translation>
+    </message>
+    <message>
+        <location filename="fractalpooluser.cc" line="508"/>
+        <source>About Fractal Pool User</source>
+        <translation type="unfinished">Om Fractal Pool User</translation>
+    </message>
+</context>
+</TS>
