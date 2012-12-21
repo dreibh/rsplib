57d693062192a14ec0efab02817ddb576bf0d171
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2012-12-21 14:23:23 +0000

    Updated autogen.sh to include fortification options for Debian/Ubuntu systems.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rsplib2@2734 516812f1-d3fd-0310-9ea4-9aededebcb87

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/bin/bash -e
+
+if [ -e /usr/bin/dpkg-buildflags ] ; then
+   export CPPFLAGS=`dpkg-buildflags --get CPPFLAGS`
+   export CFLAGS=`dpkg-buildflags --get CFLAGS`
+   export CXXFLAGS=`dpkg-buildflags --get CXXFLAGS`
+   export LDFLAGS=`dpkg-buildflags --get LDFLAGS`
+fi
 
 ./bootstrap && \
-env CC=g++ ./configure --disable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --disable-hsmgtverify --enable-test-programs $@ && \
+env CC=g++ ./configure --enable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --disable-hsmgtverify --enable-test-programs $@ && \
 ( gmake || make )
