a6fc437bcc9c5489f97552e288fb10cad10d70ee
Thomas Dreibholz <dreibh@simula.no>
2017-10-29 10:36:26 +0100

    Improved autogen.sh: use multiple cores for building under FreeBSD.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 if [ -e /usr/bin/dpkg-buildflags ] ; then
    export CPPFLAGS=`dpkg-buildflags --get CPPFLAGS`
@@ -7,6 +7,18 @@ if [ -e /usr/bin/dpkg-buildflags ] ; then
    export LDFLAGS=`dpkg-buildflags --get LDFLAGS`
 fi
 
+# ------ Obtain number of cores ---------------------------------------------
+# Try Linux
+cores=`getconf _NPROCESSORS_ONLN 2>/dev/null`
+if [ "$cores" == "" ] ; then
+   # Try FreeBSD
+   cores=`sysctl -a | grep 'hw.ncpu' | cut -d ':' -f2 | tr -d ' '`
+fi
+if [ "$cores" == "" ] ; then
+   cores="1"
+fi
+
+# ------ Configure and build ------------------------------------------------
 ./bootstrap && \
 ./configure --enable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --disable-hsmgtverify --enable-test-programs $@ && \
-( gmake || make )
+( gmake -j$cores || make -j$cores )
