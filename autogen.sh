7c94a348151a117a0447c193467f87c5bee5c493
Thomas Dreibholz <dreibh@simula.no>
2018-06-07 15:51:35 +0200

    Script updates.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
-if [ -e /usr/bin/dpkg-buildflags ] ; then
-   export CPPFLAGS=`dpkg-buildflags --get CPPFLAGS`
-   export CFLAGS=`dpkg-buildflags --get CFLAGS`
-   export CXXFLAGS=`dpkg-buildflags --get CXXFLAGS`
-   export LDFLAGS=`dpkg-buildflags --get LDFLAGS`
-fi
+rm -f CMakeCache.txt
 
 # ------ Obtain number of cores ---------------------------------------------
 # Try Linux
@@ -19,6 +14,5 @@ if [ "$cores" == "" ] ; then
 fi
 
 # ------ Configure and build ------------------------------------------------
-./bootstrap && \
-./configure --enable-kernel-sctp --enable-static --disable-shared --enable-qt --enable-csp --disable-hsmgtverify --enable-test-programs $@ && \
-( gmake -j$cores || make -j$cores )
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_QT=1 .
+gmake -j$cores || make -j$cores
