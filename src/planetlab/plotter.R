6fc6c8e87c3979e6a6dc30d70584bf01e8ff7afe
Thomas Dreibholz <dreibh@simula.no>
2022-12-14 15:43:23 +0100

    Header updates.

--- src/planetlab/plotter.R
+++ src/planetlab/plotter.R
@@ -1,7 +1,7 @@
 # $Id$
 # ###########################################################################
 #             Thomas Dreibholz's R Simulation Scripts Collection
-#                  Copyright (C) 2005-2022 Thomas Dreibholz
+#                  Copyright (C) 2005-2023 Thomas Dreibholz
 #
 #               Author: Thomas Dreibholz, dreibh@iem.uni-due.de
 # ###########################################################################
